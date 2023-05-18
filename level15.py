ee5101ca5d6860c3d0df6ad49c40b0c5f7199de8
Lee Jihun <jihun4889@gmail.com>
2023-05-18 23:17:57 +0900

    baekjoon level 15 test (ing)

---
+++ level15.py
@@ -0,0 +1,153 @@
+# %%
+# q1
+import sys
+T = int(input())
+t_list = [list(map(int, sys.stdin.readline().split())) for _ in range(T)]
+a = [t[0] for t in t_list]
+b = [t[1] for t in t_list]
+
+for i in range(T):
+    c = max(a[i], b[i])
+    d = min(a[i], b[i])
+    for j in range(c, a[i] * b[i]+1, c):
+        if j % d == 0:
+            print(j)
+            break
+
+# %%
+# q2
+a, b = map(int, input().split())
+c = max(a, b)
+d = min(a, b)
+for j in range(c, a * b+1, c):
+    if j % d == 0:
+        print(j)
+        break
+
+# %%
+# q3
+def primes(n1, n2):
+    prime = max(n1, n2)
+    n_max = min(n1, n2)
+    while prime != 0:
+        remainder = n_max % prime
+        n_max = prime
+        prime = remainder
+
+    return n_max
+
+a1, b1 = map(int, input().split())
+a2, b2 = map(int, input().split())
+
+a = a1 * b2 + a2 * b1
+b = b1 * b2
+
+c = primes(a, b)
+
+print(a//c, b//c)
+
+# %%
+# q4
+import math
+n = int(input())
+n_list = sorted([int(input()) for _ in range(n)])
+n_set = set()
+for i in range(1, n):
+    n_set.add(n_list[i] - n_list[i-1])
+
+n_set = list(n_set)
+
+prime = []
+if len(n_set) > 1:
+    for j in range(1, len(n_set)):
+        prime.append(math.gcd(n_set[j-1], n_set[j]))
+else:
+    prime = n_set
+print((n_list[-1] - n_list[0]) // min(prime) - (n - 1))
+
+# %%
+# q5
+import math, sys
+def next_primes(num):
+    while True:
+        if num < 2:
+            num = 2
+        prime = []
+        for i in range(2, int(num**0.5)+1):
+            if num % i == 0:
+                prime.append(i)
+                prime.append(num//i)
+                break
+        if len(prime) == 0:
+            return num
+        num += 1
+
+n = int(input())
+n_list = [int(input()) for _ in range(n)]
+for i in n_list:
+    print(next_primes(i))
+
+# %%
+# q6
+M, N = map(int, input().split())
+n_list = list(range(max(2, M)), N+1)
+for i in range(M, N+1):
+    cnt = 0
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            cnt += 1
+            break
+    if cnt == 0:
+        print(i)
+
+# %%
+# q7
+def primes_length(num):
+    array = [False, False] + [True] * (num*2-1)
+    # while True:
+    for i in range(2, num*2+1):
+        if array[i]:
+            j = 2
+            while i * j <= num*2:
+                array[i * j] = False
+                j += 1
+        
+    return sum(array[num+1:num*2+1])
+    
+n_list = []
+while 0 not in n_list:
+    n_list.append(int(input()))
+
+del n_list[-1]
+
+for n in n_list:
+    print(primes_length(n))
+
+# %%
+# q8
+def primes_length(num):
+    array = [False, False] + [True] * (num*2-1)
+    
+    for i in range(2, num+1):
+        if array[i]:
+            j = 2
+            while i * j <= num*2:
+                array[i * j] = False
+                j += 1
+    
+    result_list = [array.index(j) for j in array if j]
+    cnt = 0
+    for r in result_list:
+        if num-r in result_list:
+            cnt += 1
+
+    return cnt // 2
+
+N = int(input())
+n_list = [int(input()) for _ in range(N)]
+for n in n_list:
+
+
+# %%
+test = [True, True, False]
+print(sum(test))
